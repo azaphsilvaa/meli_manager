1da106fff110f21554cf82b513b04df96c753815
azaphsilvaa <azaphsilva2024@gmail.com>
2026-03-30 20:55:27 -0300

    update

--- app/config/app_info.py
+++ app/config/app_info.py
@@ -2,7 +2,7 @@ APP_NAME = "SOFTWARE ML"
 APP_VERSION = "1.0.0"
 
 GITHUB_OWNER = "azaphsilvaa"
-GITHUB_REPOSITORY = "software-ml"
+GITHUB_REPOSITORY = "meli_manager"
 
 GITHUB_API_LATEST_RELEASE = (
     f"https://api.github.com/repos/{GITHUB_OWNER}/{GITHUB_REPOSITORY}/releases/latest"
